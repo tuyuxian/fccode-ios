4072b5c0ee9e3ef3e50eb60ec6265591da46dfb7
Sam Tu <cylt2212@gmail.com>
2023-04-06 03:30:43 -0700

    Init

---
+++ FingerCrossed/FingerCrossed/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  FingerCrossed
+//
+//  Created by Yu-Hsien Tu on 4/6/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FingerCrossed/FingerCrossed/FingerCrossedApp.swift
@@ -0,0 +1,17 @@
+//
+//  FingerCrossedApp.swift
+//  FingerCrossed
+//
+//  Created by Yu-Hsien Tu on 4/6/23.
+//
+
+import SwiftUI
+
+@main
+struct FingerCrossedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

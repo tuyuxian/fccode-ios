737f2bf8d5d713d7a5275e15d681020af5dd16f6
Sam Tu <cylt2212@gmail.com>
2023-04-09 23:34:59 -0700

    feat(profile): add preference view
    Main changes:
    - Add series of preference views

---
+++ FingerCrossed/FingerCrossed/Core/Profile/Preference/PreferenceEthnicityView.swift
@@ -0,0 +1,54 @@
+//
+//  PreferenceEthnicityView.swift
+//  FingerCrossed
+//
+//  Created by Yu-Hsien Tu on 4/9/23.
+//
+
+import SwiftUI
+
+struct PreferenceEthnicityView: View {
+    
+    let ethnicityOptions:[String] = [
+        "Open to all",
+        "Asian",
+        "Black/African-descent",
+        "Hispanic/Latino",
+        "Native American",
+        "Pacifci Islander",
+        "South Asian",
+        "White/Caucasian",
+    ]
+
+    var body: some View {
+        ContainerWithHeaderView(parentTitle: "Preference", childTitle: "Ethnicity") {
+            Box {
+                VStack(spacing: 0) {
+                    ForEach(Array(ethnicityOptions.enumerated()), id: \.element.self) { index, ethnicity in
+                        HStack {
+                            CheckboxButton(label: ethnicity) // TODO(Sam): add click state
+                            Spacer()
+                        }
+                        .padding(EdgeInsets(top: 20, leading: 24, bottom: 20, trailing: 24))
+                       
+                        
+                        index != ethnicityOptions.count - 1
+                        ? Divider().foregroundColor(Color.surface2) // TODO(Sam): use surface3
+                                .padding(EdgeInsets(top: 0, leading: 24, bottom: 0, trailing: 24))
+                        : nil
+                        
+                    }
+                }
+                .padding(EdgeInsets(top: 10, leading: 0, bottom: 0, trailing: 0))
+                Spacer()
+                TabBar()
+            }
+        }
+    }
+}
+
+struct PreferenceEthnicityView_Previews: PreviewProvider {
+    static var previews: some View {
+        PreferenceEthnicityView()
+    }
+}
---
+++ FingerCrossed/FingerCrossed/Core/Profile/Preference/PreferenceSexOrientationView.swift
@@ -0,0 +1,50 @@
+//
+//  PreferenceSexOrientationView.swift
+//  FingerCrossed
+//
+//  Created by Yu-Hsien Tu on 4/9/23.
+//
+
+import SwiftUI
+
+struct PreferenceSexOrientationView: View {
+    
+    let sexOrientationOptions:[String] = [
+        "Heterosexuality",
+        "Bisexuality",
+        "Homosexuality",
+        "All",
+    ]
+    
+    var body: some View {
+        ContainerWithHeaderView(parentTitle: "Preference", childTitle: "Sex Orientation") {
+            Box {
+                VStack(spacing: 0) {
+                    ForEach(Array(sexOrientationOptions.enumerated()), id: \.element.self) { index, sexOrientation in
+                        HStack {
+                            CheckboxButton(label: sexOrientation) // TODO(Sam): add click state
+                            Spacer()
+                        }
+                        .padding(EdgeInsets(top: 20, leading: 24, bottom: 20, trailing: 24))
+                        
+                        
+                        index != sexOrientationOptions.count - 1
+                        ? Divider().foregroundColor(Color.surface2) // TODO(Sam): use surface3
+                            .padding(EdgeInsets(top: 0, leading: 24, bottom: 0, trailing: 24))
+                        : nil
+                        
+                    }
+                }
+                .padding(EdgeInsets(top: 10, leading: 0, bottom: 0, trailing: 0))
+                Spacer()
+                TabBar()
+            }
+        }
+    }
+}
+
+struct PreferenceSexOrientationView_Previews: PreviewProvider {
+    static var previews: some View {
+        PreferenceSexOrientationView()
+    }
+}
